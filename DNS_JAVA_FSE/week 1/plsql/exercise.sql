0b6253ae3dd088c4b89cd9cabdd69d8003f847a7
Arihant Sharma <arihantsharma51@gmail.com>
2026-06-21 20:04:33 +0530

    new1

--- DNS_JAVA_FSE/week 1/plsql/exercise.sql
+++ DNS_JAVA_FSE/week 1/plsql/exercise.sql
@@ -1,5 +1,6 @@
 set SERVEROUTPUT on;    
-DECLARE
+
+DECLARE------------------------------------------------------------1
     CURSOR c
 IS
     select CUSTOMERID,DOB from CUSTOMERS;
@@ -17,4 +18,50 @@ BEGIN
     end loop;
 end;
 /
-SELECT * from LOANS;
+
+SELECT * from LOANS,CUSTOMERS,TRANSACTIONS,ACCOUNTS,EMPLOYEES;
+
+DECLARE-----------------------------------------------------------------------2
+    CURSOR C
+IS
+    SELECT CUSTOMERID,BALANCE from CUSTOMERS;
+BEGIN
+    for r in c LOOP
+        if r.balance>10000 THEN
+        update CUSTOMERS
+        set ISVIP='TRUE'
+        where CUSTOMERID=r.CUSTOMERID;
+
+        dbms_output.put_line('update'||r.CUSTOMERID);
+        end if;
+    end loop;
+end;
+/
+
+desc CUSTOMERS;
+
+ALTER TABLE Customers
+ADD ISVIP VARCHAR2(10);
+commit;
+
+UPDATE Customers
+SET Balance=15000
+WHERE CustomerID=1;
+COMMIT;
+
+DECLARE
+    CURSOR C
+IS
+    select C.CUSTOMERID,C.NAME,l.ENDDATE from CUSTOMERS C
+    join LOANS l
+    on C.CUSTOMERID = L.CUSTOMERID
+    WHERE L.ENDDATE BETWEEN SYSDATE
+    AND SYSDATE+30;
+begin 
+    for r in c LOOP
+    DBMS_OUTPUT.PUT_LINE('REMINDER'||R.NAME||'LOAN DUE ON'||TO_CHAR (r.ENDDATE,'DD-MM-YYYY'));
+    END LOOP;
+END;
+/
+COMMIT;
+
