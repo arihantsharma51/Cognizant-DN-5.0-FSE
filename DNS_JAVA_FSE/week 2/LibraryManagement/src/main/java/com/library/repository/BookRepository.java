0ab03acd9a826e5f1d467126a02f439e27d65ebb
Arihant Sharma <arihantsharma51@gmail.com>
2026-06-27 23:41:51 +0530

    spring_core

---
+++ DNS_JAVA_FSE/week 2/LibraryManagement/src/main/java/com/library/repository/BookRepository.java
@@ -0,0 +1,15 @@
+package com.library.repository;
+
+public class BookRepository {
+
+    public BookRepository() {
+        System.out.println("BookRepository Bean Created");
+    }
+
+    public void saveBook() {
+
+        System.out.println("Book saved successfully.");
+
+    }
+
+}
---
+++ DNS_JAVA_FSE/week 2/LibraryManagement/src/main/java/com/library/service/BookService.java
@@ -0,0 +1,17 @@
+package com.library.service;
+
+public class BookService {
+
+    public BookService() {
+
+        System.out.println("BookService Bean Created");
+
+    }
+
+    public void displayBook() {
+
+        System.out.println("Displaying Book Details");
+
+    }
+
+}
