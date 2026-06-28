faa652c69b8ed941f5725ff5319027a895b1dbce
Arihant Sharma <arihantsharma51@gmail.com>
2026-06-28 15:51:14 +0530

    further

--- DNS_JAVA_FSE/week 2/LibraryManagement/src/main/java/com/library/service/BookService.java
+++ DNS_JAVA_FSE/week 2/LibraryManagement/src/main/java/com/library/service/BookService.java
@@ -1,17 +1,26 @@
 package com.library.service;
 
+import com.library.repository.BookRepository;
+
 public class BookService {
 
+    // Dependency
+    private BookRepository bookRepository;
+
     public BookService() {
-
         System.out.println("BookService Bean Created");
+    }
 
+    // Setter Injection
+    public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
     }
 
     public void displayBook() {
 
         System.out.println("Displaying Book Details");
 
-    }
+        bookRepository.saveBook();
 
+    }
 }
