10fa134116babb228bd38d4e5eb3ef796662697d
Arihant Sharma <arihantsharma51@gmail.com>
2026-07-01 23:23:55 +0530

    exercise5

--- DNS_JAVA_FSE/week 2/Spring Data JPA with Hibernate/EmployeeManagementSystem (1)/EmployeeManagementSystem/src/main/java/com/employee/repository/DepartmentRepository.java
+++ DNS_JAVA_FSE/week 2/Spring Data JPA with Hibernate/EmployeeManagementSystem (1)/EmployeeManagementSystem/src/main/java/com/employee/repository/DepartmentRepository.java
@@ -1,5 +1,7 @@
 package com.employee.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import com.employee.entity.Department;
 
 @Repository
 public interface DepartmentRepository extends JpaRepository<Department, Long> {
+
+    List<Department> findByName(String name);
 
 }
