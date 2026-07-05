9a1fb77a1af8c8ac71eb8cf774942a4a6eb15267
arihantsharma51 <arihantsharmaa51@gmail.com>
2026-07-05 19:52:52 +0530

    testing exercise 1

--- DNS_JAVA_FSE/week 3/JUnit, Mockito and SL4J/spring-testing/src/test/java/spring_testing/service/CalculatorServiceTest.java
+++ DNS_JAVA_FSE/week 3/JUnit, Mockito and SL4J/spring-testing/src/test/java/spring_testing/service/CalculatorServiceTest.java
@@ -1,19 +1,64 @@
 package spring_testing.service;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 public class CalculatorServiceTest {
 
-    CalculatorService service = new CalculatorService();
+    CalculatorService service;
+
+    @BeforeAll
+    static void beforeAllTests() {
+        System.out.println("===== Starting Calculator Tests =====");
+    }
+
+    @BeforeEach
+    void setUp() {
+        service = new CalculatorService();
+        System.out.println("Object Created");
+    }
 
     @Test
     void testAdd() {
-
         int result = service.add(10, 20);
-
         assertEquals(30, result);
+    }
 
+    @Test
+    void testNotEquals() {
+        int result = service.add(10, 20);
+        assertNotEquals(50, result);
+    }
+
+    @Test
+    void testTrue() {
+        assertTrue(20 > 10);
+    }
+
+    @Test
+    void testFalse() {
+        assertFalse(20 < 10);
+    }
+
+    @Test
+    void testNotNull() {
+        assertNotNull(service);
+    }
+
+    @Test
+    void testNull() {
+        String name = null;
+        assertNull(name);
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("Test Completed");
+    }
+
+    @AfterAll
+    static void afterAllTests() {
+        System.out.println("===== All Tests Finished =====");
     }
 }
