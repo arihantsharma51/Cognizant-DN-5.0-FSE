137309c24315f802456fbcaa4edb6f69ce33afa5
arihantsharma51 <arihantsharmaa51@gmail.com>
2026-07-06 09:19:47 +0530

    module2ex1

---
+++ DNS_JAVA_FSE/week 3/JUnit, Mockito and SL4J/spring-testing/src/test/java/spring_testing/service/ExecutionOrderTest.java
@@ -0,0 +1,29 @@
+package spring_testing.service;
+
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.MethodOrderer;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class ExecutionOrderTest {
+
+    @Test
+    @Order(2)
+    void secondTest() {
+        System.out.println("Second Test Executed");
+    }
+
+    @Test
+    @Order(1)
+    void firstTest() {
+        System.out.println("First Test Executed");
+    }
+
+    @Test
+    @Order(3)
+    void thirdTest() {
+        System.out.println("Third Test Executed");
+    }
+
+}
